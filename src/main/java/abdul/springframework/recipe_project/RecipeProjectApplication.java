d0e5bf742148a991caf79f7c9b09d1fa9e44f6a2
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-06-10 10:55:06 +0100

    first commit

---
+++ src/main/java/abdul/springframework/recipe_project/RecipeProjectApplication.java
@@ -0,0 +1,12 @@
+package abdul.springframework.recipe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProjectApplication.class, args);
+	}
+}
