ddba26a744384e5efa591ef639585c526a4b8caf
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-06-11 05:31:16 +0100

    add difficulty enum

---
+++ src/main/java/abdul/springframework/recipe_project/domain/Difficulty.java
@@ -0,0 +1,8 @@
+package abdul.springframework.recipe_project.domain;
+
+/**
+ * Created by 4bdul on 11/06/2018 at 5:26 AM.
+ */
+public enum Difficulty {
+    EASY,MODERATE,HARD
+}
--- src/main/java/abdul/springframework/recipe_project/domain/Recipe.java
+++ src/main/java/abdul/springframework/recipe_project/domain/Recipe.java
@@ -21,8 +21,8 @@ public class Recipe {
     private String url;
     private String directions;
 
-    //todo add
-    //private Difficulty difficulty
+    @Enumerated(value= EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -112,5 +112,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
