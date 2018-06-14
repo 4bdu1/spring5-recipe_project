df89e71f6beeee5044bf69acca759d03d2357a9a
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-06-14 13:17:02 +0100

    add project lombok

--- src/main/java/abdul/springframework/recipe_project/domain/Category.java
+++ src/main/java/abdul/springframework/recipe_project/domain/Category.java
@@ -1,11 +1,14 @@
 package abdul.springframework.recipe_project.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Set;
 
 /**
  * Created by 4bdul on 11/06/2018 at 5:31 AM.
  */
+@Data
 @Entity
 public class Category {
 
@@ -17,28 +20,5 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipe;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Set<Recipe> recipe) {
-        this.recipe = recipe;
-    }
 }
 
--- src/main/java/abdul/springframework/recipe_project/services/RecipeServiceImpl.java
+++ src/main/java/abdul/springframework/recipe_project/services/RecipeServiceImpl.java
@@ -2,6 +2,7 @@ package abdul.springframework.recipe_project.services;
 
 import abdul.springframework.recipe_project.domain.Recipe;
 import abdul.springframework.recipe_project.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -10,6 +11,7 @@ import java.util.Set;
 /**
  * Created by 4bdul on 13/06/2018 at 5:42 PM.
  */
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService{
 
@@ -21,7 +23,7 @@ public class RecipeServiceImpl implements RecipeService{
 
     @Override
     public Set<Recipe> getRecipes() {
-
+        log.debug("I'm in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
