e269fee869a534352a7f60ab6c11053c97b75799
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-06-16 13:33:21 +0100

    test for RecipeServiceImpl

---
+++ src/test/java/abdul/springframework/recipe_project/services/RecipeServiceImplTest.java
@@ -0,0 +1,46 @@
+package abdul.springframework.recipe_project.services;
+
+import abdul.springframework.recipe_project.domain.Recipe;
+import abdul.springframework.recipe_project.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by 4bdul on 16/06/2018 at 1:25 PM.
+ */
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        Set<Recipe> recipes =  recipeService.getRecipes();
+
+        assertEquals(recipes.size(),1);
+        verify(recipeRepository,times(1)).findAll();
+    }
+}
