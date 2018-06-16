4e7526b3dd5cc9eb287c5fa109f491b89ae67c40
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-06-16 13:46:31 +0100

    test for IndexController

---
+++ src/test/java/abdul/springframework/recipe_project/controllers/IndexControllerTest.java
@@ -0,0 +1,44 @@
+package abdul.springframework.recipe_project.controllers;
+
+import abdul.springframework.recipe_project.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by 4bdul on 16/06/2018 at 1:34 PM.
+ */
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String viewName = controller.getIndexPage(model);
+
+        assertEquals("index",viewName);
+        verify(recipeService,times(1)).getRecipes();
+        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+
+    }
+}
