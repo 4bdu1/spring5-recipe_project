15f00f9f9dd99385c1d945e5c1208d1b6b97b45d
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-06-24 17:17:49 +0100

    add view ingredients and tests

---
+++ src/test/java/abdul/springframework/recipe_project/controllers/IngredientControllerTest.java
@@ -0,0 +1,54 @@
+package abdul.springframework.recipe_project.controllers;
+
+import abdul.springframework.recipe_project.commands.RecipeCommand;
+import abdul.springframework.recipe_project.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Created by 4bdul on 24/06/2018 at 5:05 PM.
+ */
+public class IngredientControllerTest {
+
+
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController controller;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new IngredientController( recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void testListIngredients() throws Exception {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //when
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        //then
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+
+}
