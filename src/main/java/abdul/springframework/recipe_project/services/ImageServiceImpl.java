71f2f512595019575a55597bcf084a2133c0954a
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-07-05 05:45:55 +0100

    save image in db

--- src/main/java/abdul/springframework/recipe_project/services/ImageServiceImpl.java
+++ src/main/java/abdul/springframework/recipe_project/services/ImageServiceImpl.java
@@ -1,8 +1,12 @@
 package abdul.springframework.recipe_project.services;
 
+import abdul.springframework.recipe_project.domain.Recipe;
+import abdul.springframework.recipe_project.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by 4bdul on 05/07/2018 at 5:19 AM.
@@ -10,8 +14,34 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl( RecipeRepository recipeService) {
+
+        this.recipeRepository = recipeService;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received a file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
     }
 }
