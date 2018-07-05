35806061bd5b96790a586ce7edbfab549056fe50
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-07-05 08:32:03 +0100

    display image from db

--- src/main/java/abdul/springframework/recipe_project/controllers/ImageController.java
+++ src/main/java/abdul/springframework/recipe_project/controllers/ImageController.java
@@ -1,7 +1,9 @@
 package abdul.springframework.recipe_project.controllers;
 
+import abdul.springframework.recipe_project.commands.RecipeCommand;
 import abdul.springframework.recipe_project.services.ImageService;
 import abdul.springframework.recipe_project.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by 4bdul on 05/07/2018 at 4:54 AM.
@@ -37,6 +44,24 @@ public class ImageController {
         imageService.saveImageFile(Long.valueOf(id), file);
 
         return "redirect:/recipe/" + id + "/show";
+    }
+
+    @GetMapping("recipe/{id}/recipeimage")
+    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+
+        if (recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+            int i = 0;
+
+            for (Byte wrappedByte : recipeCommand.getImage()){
+                byteArray[i++] = wrappedByte; //auto unboxing
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
     }
 
 }
